e205349a543e97aaa78a92377989a248b3a368ec
Raj <56403033+Radioactive92177@users.noreply.github.com>
2019-12-21 22:06:55 +0530

    Update RandomNumberGame.py

--- RandomNumberGame.py
+++ RandomNumberGame.py
@@ -1,16 +1,17 @@
 import random
 
+
 def star():
     for i in range(20):
-        print('*',end='')
+        print('*', end='')
 
 
 star()
-print("LETS START THE GAME",end='')
+print("LETS START THE GAME", end='')
 star()
 print('')
 
-#Player Input
+# Player Input
 Start = int(input('Enter the Starting point  : '))
 
 End = int(input('Enter the Ending point : '))
@@ -19,56 +20,52 @@ Guess = int(input('Guess the number = '))
 
 space = End - Start
 Points = 0
-if space <=100 and space>90:
+if 100 >= space > 90:
     Points = 100
     print('Your got 100 Points')
-elif space <=90 and space>80:
+elif 90 >= space > 80:
     Points = 90
     print('Your got 90 Points')
-elif space <=80 and space>70:
+elif 80 >= space > 70:
     Points = 80
     print('Your got 80 Points')
-elif space <=70 and space>60:
+elif 70 >= space > 60:
     Points = 70
     print('Your got 70 Points')
-elif space <=60 and space>50:
+elif 60 >= space > 50:
     Points = 60
     print('Your got 60 Points')
-elif space <=50 and space>40:
+elif 50 >= space > 40:
     Points = 50
     print('Your got 50 Points')
-elif space <=40 and space>30:
+elif 40 >= space > 30:
     Points = 40
     print('Your got 40 Points')
-elif space <=30 and space>20:
+elif 30 >= space > 20:
     Points = 30
     print('Your got 30 Points')
-elif space <=20 and space>10:
+elif 20 >= space > 10:
     Points = 20
     print('Your got 20 Points')
 else:
     Points = 10
     print('Your got 10 Points')
 
-
-
-#Random number generator
+# Random number generator
 Number = random.randint(Start, End)
 
-
-
-#Mechanism
-while(Number!="Guess"):
-    if Guess>Number:
-         print('You are a bit higher than the no')
-         Guess = int(input('Guess the number again = '))
-         Points = Points-10
-         if Points == 0:
-             print('NOT ENOUGH POINTS')
-             break
-         else:
-             print(Points)
-    elif Guess<Number:
+# Mechanism
+while Number != "Guess":
+    if Guess > Number:
+        print('You are a bit higher than the no')
+        Guess = int(input('Guess the number again = '))
+        Points = Points - 10
+        if Points == 0:
+            print('NOT ENOUGH POINTS')
+            break
+        else:
+            print(Points)
+    elif Guess < Number:
         print('You are a bit lower than the number')
         Guess = int(input('Guess the number again = '))
         Points = Points - 10
@@ -78,8 +75,8 @@ while(Number!="Guess"):
         else:
             print(Points)
     else:
-         print('You guessed the right no')
-         star()
-         print('GAME OVER',end='')
-         star()
-         break
+        print('You guessed the right no')
+        star()
+        print('GAME OVER', end='')
+        star()
+        break
